8f103ea488d6c8a1d3e40a656423ed36a9be4d84
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-11-19 11:29:10 -0300

    Desafio concluído

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -1,6 +1,8 @@
 package br.com.dio.desafio;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,8 +26,41 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+        */
+
+        Dev devFulano = new Dev();
+        devFulano.setNome("Fulano");
+        devFulano.inscreverBootcamp(bootcamp);
+        devFulano.progredir();
+        devFulano.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Fulano: " + devFulano.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Fulano: " + devFulano.getConteudosConcluidos());
+        System.out.println("XP: " + devFulano.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devSicrano = new Dev();
+        devSicrano.setNome("Sicrano");
+        devSicrano.inscreverBootcamp(bootcamp);
+        devSicrano.progredir();
+        devSicrano.progredir();
+        devSicrano.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Sicrano: " + devSicrano.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Sicrano: " + devSicrano.getConteudosConcluidos());
+        System.out.println("XP: " + devSicrano.calcularTotalXp());
     }
+
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,31 +1,18 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getCargaHoraria() {
+   public int getCargaHoraria() {
         return cargaHoraria;
     }
 
@@ -36,9 +23,10 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
+
 }
